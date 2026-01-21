0fe9f284f759ad898fc454058a88eb012b35d615
kayarhabby <pasciahershekaya@gmail.com>
2026-01-21 17:43:15 +0100

    add image service

---
+++ src/main/java/com/example/vitrinesitepoker/service/ImageService.java
@@ -0,0 +1,11 @@
+package com.example.vitrinesitepoker.service;
+
+import com.example.vitrinesitepoker.model.Image;
+import java.util.List;
+import java.util.Optional;
+
+public interface ImageService {
+    List<Image> getAllImages();
+    Optional<Image> getImage(int id);
+}
+
---
+++ src/main/java/com/example/vitrinesitepoker/service/ImageServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.vitrinesitepoker.service;
+
+import com.example.vitrinesitepoker.model.Image;
+import com.example.vitrinesitepoker.repository.ImageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository imageRepository;
+
+    public ImageServiceImpl(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    @Override
+    public List<Image> getAllImages() {
+        return imageRepository.findAll();
+    }
+    
+    @Override
+    public Optional<Image> getImage(int id){
+        return imageRepository.findById(id);
+    }
+}
+
