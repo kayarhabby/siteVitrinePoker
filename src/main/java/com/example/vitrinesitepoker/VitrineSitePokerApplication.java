e00668a213a75160675bda09668d81e46a30116e
kayarhabby <pasciahershekaya@gmail.com>
2026-01-21 15:02:17 +0100

    project init

---
+++ src/main/java/com/example/vitrinesitepoker/VitrineSitePokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vitrinesitepoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrineSitePokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitrineSitePokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vitrinesitepoker/VitrineSitePokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitrinesitepoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitrineSitePokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
