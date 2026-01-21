66635db57ef402c329de473ae2f3ee1a45673778
kayarhabby <pasciahershekaya@gmail.com>
2026-01-21 17:42:23 +0100

    image model

---
+++ src/main/java/com/example/vitrinesitepoker/model/Image.java
@@ -0,0 +1,24 @@
+package com.example.vitrinesitepoker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String titre;
+
+    private String url;
+}
