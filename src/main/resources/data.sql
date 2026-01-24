4cad1a3db392c61e7154459785d925905c71dd76
kayarhabby <pasciahershekaya@gmail.com>
2026-01-24 22:40:02 +0100

    update files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO IMAGE (TITRE, URL) VALUES ('Poker Table', 'poker1.jpg');
-INSERT INTO IMAGE (TITRE, URL) VALUES ('Blackjack Night', 'blackjack.jpg');
-INSERT INTO IMAGE (TITRE, URL) VALUES ('Roulette Wheel', 'roulette.jpg');
-INSERT INTO IMAGE (TITRE, URL) VALUES ('VIP Lounge', 'vip-lounge.jpg');
-INSERT INTO IMAGE (TITRE, URL) VALUES ('Casino Entrance', 'casino-entrance.jpg');
+INSERT INTO image (TITRE, URL) VALUES ('Poker Table', 'poker1.jpg');
+INSERT INTO image (TITRE, URL) VALUES ('Blackjack Night', 'blackjack.jpg');
+INSERT INTO image (TITRE, URL) VALUES ('Roulette Wheel', 'roulette.jpg');
+INSERT INTO image (TITRE, URL) VALUES ('VIP Lounge', 'vip-lounge.jpg');
+INSERT INTO image (TITRE, URL) VALUES ('Casino Entrance', 'casino-entrance.jpg');
